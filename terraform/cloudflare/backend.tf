67a4098356896682334514c934afee5ac093cf29
Keitaro Takeuchi <tiyotiyouda@gmail.com>
2024-02-21 00:13:15 +0900

    fix: cloudflare to correct

---
+++ terraform/cloudflare/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "remote" {
+    organization = "hakoniwa"
+
+    workspaces {
+      name = "cloudflare"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4"
+    }
+  }
+}
