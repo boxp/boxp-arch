117b80e0d105e40c7a1dbd6f5b873a9e0636f3c8
Keitaro Takeuchi <tiyotiyouda@gmail.com>
2024-02-21 01:04:57 +0900

    add: imported resources

---
+++ terraform/cloudflare/b0xp.io/dns/variable.tf
@@ -0,0 +1,9 @@
+variable "domain" {
+  type    = string
+  default = "b0xp.io"
+}
+
+variable "zone" {
+  type    = string
+  default = "ec593206d0ef695c3aae3a4cb3173264"
+}
--- terraform/cloudflare/provider.tf
+++ terraform/cloudflare/provider.tf
@@ -1,3 +1,3 @@
 provider "cloudflare" {
-  api_token = var.cloudflare_api_token
+  # token pulled from $CLOUDFLARE_API_TOKEN
 }
