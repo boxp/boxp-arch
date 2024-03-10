e304c4fb7c385161fadb38620c8b714275aeb01c
boxp <tiyotiyouda@gmail.com>
2024-03-10 22:39:28 +0900

    fix: to commented out

--- terraform/aws/tfaction/iam.tf
+++ terraform/aws/tfaction/iam.tf
@@ -9,23 +9,25 @@ module "aws" {
 }
 
 # Attach Policies
+# tfaction 導入に必要だったresourcesたち
+# tfmigrate で import するのに挫折したのでメモだけ置いておく
 
-resource "aws_iam_role_policy_attachment" "terraform_apply_admin" {
-  role       = module.aws.aws_iam_role_terraform_apply_name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "terraform_plan_readonly" {
-  role       = module.aws.aws_iam_role_terraform_plan_name
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "tfmigrate_plan_readonly" {
-  role       = module.aws.aws_iam_role_tfmigrate_plan_name
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "tfmigrate_apply_readonly" {
-  role       = module.aws.aws_iam_role_tfmigrate_apply_name
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
+# resource "aws_iam_role_policy_attachment" "terraform_apply_admin" {
+#   role       = module.aws.aws_iam_role_terraform_apply_name
+#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+# }
+# 
+# resource "aws_iam_role_policy_attachment" "terraform_plan_readonly" {
+#   role       = module.aws.aws_iam_role_terraform_plan_name
+#   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+# }
+# 
+# resource "aws_iam_role_policy_attachment" "tfmigrate_plan_readonly" {
+#   role       = module.aws.aws_iam_role_tfmigrate_plan_name
+#   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+# }
+# 
+# resource "aws_iam_role_policy_attachment" "tfmigrate_apply_readonly" {
+#   role       = module.aws.aws_iam_role_tfmigrate_apply_name
+#   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+# }
