8e97010a384c1d4cfc75b8a149a8ab5d04c31011
boxp <tiyotiyouda@gmail.com>
2024-03-11 01:21:02 +0900

    fix: policy to group policy

--- terraform/aws/external-secrets-operator/iam.tf
+++ terraform/aws/external-secrets-operator/iam.tf
@@ -1,10 +1,22 @@
-resource "aws_iam_user" "external_secrets_operator_user" {
-  name = "exteral-secrets-operator-user" # ユーザー名を適切なものに変更してください
+resource "aws_iam_group" "external_secrets_operator_group" {
+    name = "external-secrets-operator-group"
 }
 
-resource "aws_iam_role" "external_secrets_role" {
-  name = "external-secrets-role"
-  assume_role_policy = jsonencode({
+resource "aws_iam_user" "external_secrets_operator_user" {
+  name = "exteral-secrets-operator-user"
+}
+
+resource "aws_iam_group_membership" "external_secrets_operator_membership" {
+  name = "external-secrets-operator-membership"
+  users = [aws_iam_user.external_secrets_operator_user.name]
+  group = aws_iam_group.external_secrets_operator_group.name
+}
+
+resource "aws_iam_group_policy" "external_secrets_policy" {
+  name               = "external-secrets-policy"
+  group = aws_iam_group.external_secrets_operator_group.name
+
+  policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
       {
@@ -16,15 +28,4 @@ resource "aws_iam_role" "external_secrets_role" {
       }
     ]
   })
-}
-
-resource "aws_iam_policy_attachment" "external_secrets_policy_attachment" {
-  name       = "external-secrets-policy-attachment"
-  roles      = [aws_iam_role.external_secrets_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess" # External Secrets OperatorがSSMにアクセスするためのポリシー
-}
-
-resource "aws_iam_user_policy_attachment" "attach" {
-  user       = aws_iam_user.external_secrets_operator_user.name
-  policy_arn = aws_iam_role.external_secrets_role.arn
 }
