928be594d2d68785d93e4485297e3a4c6c8a1745
boxp <tiyotiyouda@gmail.com>
2024-03-11 22:37:14 +0900

    fix: to didn't attach role

--- terraform/aws/external-secrets-operator/iam.tf
+++ terraform/aws/external-secrets-operator/iam.tf
@@ -1,33 +1,42 @@
-# ESO が accessToken を使って認証のみをサポートしている様子なので、このポリシーは諦める
-# tfivy:ignore:AVD-AWS-0123
-resource "aws_iam_group" "external_secrets_operator_group" {
-  name = "external-secrets-operator-group"
-}
-
-resource "aws_iam_user" "external_secrets_operator_user" {
-  name = "exteral-secrets-operator-user"
-}
-
-resource "aws_iam_group_membership" "external_secrets_operator_membership" {
-  name  = "external-secrets-operator-membership"
-  users = [aws_iam_user.external_secrets_operator_user.name]
-  group = aws_iam_group.external_secrets_operator_group.name
-}
-
-resource "aws_iam_group_policy" "external_secrets_policy" {
-  name  = "external-secrets-policy"
-  group = aws_iam_group.external_secrets_operator_group.name
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ssm:GetParameters"
-        ],
-        "Resource" : "arn:aws:ssm:*:*:parameter/*"
-      }
+# External Secret OperatorがSSMを読み取るためのIAMポリシー
+data "aws_iam_policy_document" "external_secret_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath"
     ]
+    resources = [
+            "arn:aws:ssm:ap-northeast-1:839695154978:parameter/eso-dev-*",
+            "arn:aws:ssm:ap-northeast-1:839695154978:parameter/eso-stage-*",
+            "arn:aws:ssm:ap-northeast-1:839695154978:parameter/eso-prod-*"
+    ]
+  }
+}
+
+# External Secret OperatorがアタッチするIAMロール
+resource "aws_iam_role" "external_secrets_role" {
+  name               = "external_secrets_role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        AWS = "*"
+      },
+      Action    = "sts:AssumeRole"
+    }]
   })
+}
+
+# 上記ポリシーをロールにアタッチ
+resource "aws_iam_role_policy_attachment" "external_secrets_policy_attachment" {
+  role       = aws_iam_role.external_secrets_role.name
+  policy_arn = aws_iam_policy.external_secret_policy.arn
+}
+
+# External Secret Operatorが利用するIAMユーザー
+resource "aws_iam_user" "external_secrets_user" {
+  name = "external_secrets_user"
 }
