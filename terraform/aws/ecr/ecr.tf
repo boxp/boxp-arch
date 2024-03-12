a0bbe633245c57c9ebb8914841ba98427b029ee3
boxp <tiyotiyouda@gmail.com>
2024-03-13 02:11:25 +0900

    fix: for trivy

--- terraform/aws/ecr/ecr.tf
+++ terraform/aws/ecr/ecr.tf
@@ -1,12 +1,13 @@
 resource "aws_ecr_repository" "boxp" {
   name                 = "boxp"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
 
   encryption_configuration {
-    encryption_type = "AES256"
+      encryption_type = "KMS"
+      kms_key = aws_kms_key.ecr_kms.key_id
   }
 }
