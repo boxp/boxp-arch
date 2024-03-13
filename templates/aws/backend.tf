ec458bb87e39d4212fec619de90e9b282391ef87
boxp <tiyotiyouda@gmail.com>
2024-03-14 03:06:41 +0900

    fix: backend to s3

--- templates/aws/backend.tf
+++ templates/aws/backend.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 1.0"
+  backend "s3" {
+    bucket = "tfaction-state"
+    key    = "%%TARGET%%/v1/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
