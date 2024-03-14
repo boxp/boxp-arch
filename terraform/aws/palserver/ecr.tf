645dbd3cf0b7191dfea1de3dc2c286dd75c93f46
boxp <tiyotiyouda@gmail.com>
2024-03-14 23:41:23 +0900

    fix: to default key

--- terraform/aws/palserver/ecr.tf
+++ terraform/aws/palserver/ecr.tf
@@ -1,6 +1,3 @@
-resource "aws_kms_key" "ecr_kms" {
-  enable_key_rotation = true
-}
 resource "aws_ecr_repository" "palserver" {
   name                 = "palserver"
   image_tag_mutability = "IMMUTABLE"
@@ -11,6 +8,5 @@ resource "aws_ecr_repository" "palserver" {
 
   encryption_configuration {
     encryption_type = "KMS"
-    kms_key         = aws_kms_key.ecr_kms.key_id
   }
 }
