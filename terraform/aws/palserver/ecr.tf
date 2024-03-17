63e9ea232e29af4c06797186e5fb57f4874518da
boxp <tiyotiyouda@gmail.com>
2024-03-17 22:05:30 +0900

    remove: temporary ecr

--- terraform/aws/palserver/ecr.tf
+++ terraform/aws/palserver/ecr.tf
@@ -1,14 +1,14 @@
-resource "aws_ecr_repository" "palserver" {
-  name                 = "palserver"
-  image_tag_mutability = "IMMUTABLE"
+# resource "aws_ecr_repository" "palserver" {
+#   name                 = "palserver"
+#   image_tag_mutability = "IMMUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+#   image_scanning_configuration {
+#     scan_on_push = true
+#   }
 
-  # terraform planが非常に不安定になったのでkms keyはdefaultにする
-  #trivy:ignore:AVD-AWS-0033
-  encryption_configuration {
-    encryption_type = "KMS"
-  }
-}
+#   # terraform planが非常に不安定になったのでkms keyはdefaultにする
+#   #trivy:ignore:AVD-AWS-0033
+#   encryption_configuration {
+#     encryption_type = "KMS"
+#   }
+# }
