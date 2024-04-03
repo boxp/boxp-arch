60229e1d036ec137d747607f93e73db4a0bff41d
boxp <tiyotiyouda@gmail.com>
2024-04-04 02:18:10 +0900

    remove: dns

--- terraform/cloudflare/b0xp.io/dns/pal.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_record" "terraform_managed_resource_1e2fbbba66fdf0c6df71effb39d15b9c" {
-  name    = "pal"
-  proxied = true
-  ttl     = 1
-  type    = "CNAME"
-  value   = "2416b3df-cdba-4370-a0c4-9a50aec1590c.cfargotunnel.com"
-  zone_id = var.zone
-}
