1d3e1f06e30093b129f94a2a76a7fad55bbd6bae
boxp <tiyotiyouda@gmail.com>
2024-04-16 01:55:29 +0900

    fix: policy to comment out

--- terraform/cloudflare/b0xp.io/prometheus-operator/access.tf
+++ terraform/cloudflare/b0xp.io/prometheus-operator/access.tf
@@ -7,15 +7,15 @@ resource "cloudflare_access_application" "grafana" {
 }
 
 # Creates an Access policy for the application.
-resource "cloudflare_access_policy" "grafana_policy" {
-  application_id = cloudflare_access_application.grafana.id
-  zone_id        = var.zone_id
-  name           = "policy for grafana.b0xp.io"
-  precedence     = "1"
-  decision       = "allow"
-  include {
-    # github {
-    #   name = "boxp"
-    # }
-  }
-}
+# resource "cloudflare_access_policy" "grafana_policy" {
+#   application_id = cloudflare_access_application.grafana.id
+#   zone_id        = var.zone_id
+#   name           = "policy for grafana.b0xp.io"
+#   precedence     = "1"
+#   decision       = "allow"
+#   include {
+#     github {
+#       name = "boxp"
+#     }
+#   }
+# }
