8223fd77c7e586a5abe930c98173e43d4020850f
boxp <tiyotiyouda@gmail.com>
2024-10-28 02:21:54 +0900

    add: redirect to portfolio

---
+++ terraform/cloudflare/boxp.tk/portfolio/dns.tf
@@ -0,0 +1,25 @@
+resource "cloudflare_page_rule" "redirect_subdomain" {
+  zone_id  = var.zone_id                        # ドメインのzone ID
+  target   = "https://boxp.tk/*"     # リダイレクト元
+  priority = 1                                 # ルールの優先度
+
+  actions {
+    forwarding_url {
+      url         = "https://www.b0xp.io"      # リダイレクト先URL
+      status_code = 301                        # 301リダイレクト
+    }
+  }
+}
+
+resource "cloudflare_page_rule" "redirect_subdomain" {
+  zone_id  = var.zone_id                        # ドメインのzone ID
+  target   = "https://www.boxp.tk/*"     # リダイレクト元
+  priority = 1                                 # ルールの優先度
+
+  actions {
+    forwarding_url {
+      url         = "https://www.b0xp.io"      # リダイレクト先URL
+      status_code = 301                        # 301リダイレクト
+    }
+  }
+}
