c54c9a163eb3c2baf3db6450cb3855a60fa24b97
boxp <tiyotiyouda@gmail.com>
2024-12-09 08:57:31 +0900

    fix: for validation

--- terraform/cloudflare/b0xp.io/k8s/access.tf
+++ terraform/cloudflare/b0xp.io/k8s/access.tf
@@ -23,7 +23,7 @@ resource "cloudflare_zero_trust_access_policy" "github_actions_access" {
 
   include {
     github {
-      identity_provider_id = data.cloudflare_access_identity_provider.github.id
+      identity_provider_id = data.cloudflare_zero_trust_access_identity_provider.github.id
       email                = ["*"]
       subject              = var.allowed_github_oidc_subjects
     }
--- terraform/cloudflare/b0xp.io/k8s/tunnel.tf
+++ terraform/cloudflare/b0xp.io/k8s/tunnel.tf
@@ -2,15 +2,15 @@ resource "random_password" "tunnel_secret" {
   length = 64
 }
 
-resource "cloudflare_tunnel" "k8s_tunnel" {
+resource "cloudflare_zero_trust_tunnel_cloudflared" "k8s_tunnel" {
   account_id = var.account_id
   name       = "cloudflare k8s tunnel"
   secret     = sensitive(base64sha256(random_password.tunnel_secret.result))
 }
 
 # Creates the configuration for the tunnel.
-resource "cloudflare_tunnel_config" "k8s_tunnel" {
-  tunnel_id  = cloudflare_tunnel.k8s_tunnel.id
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "k8s_tunnel" {
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.k8s_tunnel.id
   account_id = var.account_id
   config {
     ingress_rule {
