982758f950ac5976092177aed38636178408bd31
Keitaro Takeuchi <tiyotiyouda@gmail.com>
2024-12-09 00:31:50 +0000

    fix: to content

--- terraform/cloudflare/b0xp.io/k8s/dns.tf
+++ terraform/cloudflare/b0xp.io/k8s/dns.tf
@@ -2,7 +2,7 @@
 resource "cloudflare_record" "k8s" {
   zone_id = var.zone_id
   name    = "k8s"
-  value   = cloudflare_zero_trust_tunnel_cloudflared.k8s_tunnel.cname
+  content = cloudflare_zero_trust_tunnel_cloudflared.k8s_tunnel.cname
   type    = "CNAME"
   proxied = true
 }
