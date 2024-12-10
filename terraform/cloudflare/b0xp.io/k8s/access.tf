4dfd708af0fdd617a3255671a58f70712f298453
boxp <tiyotiyouda@gmail.com>
2024-12-11 04:27:04 +0900

    rm: ip range policy

--- terraform/cloudflare/b0xp.io/k8s/access.tf
+++ terraform/cloudflare/b0xp.io/k8s/access.tf
@@ -13,11 +13,7 @@ resource "cloudflare_zero_trust_access_policy" "github_actions_access" {
   account_id = var.account_id
   name       = "GitHub Actions Access Policy"
   decision   = "allow"
-
-  require {
-    ip = var.allowed_github_actions_ip_ranges
-  }
-
+  
   include {
     service_token = [var.service_token_id]
   }
