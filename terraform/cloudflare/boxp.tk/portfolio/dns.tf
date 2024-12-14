f65b1056902bcb0e104c3ebebd4f3fcf2300567a
boxp <tiyotiyouda@gmail.com>
2024-12-14 13:28:12 +0900

    fix: redirection

--- terraform/cloudflare/boxp.tk/portfolio/dns.tf
+++ terraform/cloudflare/boxp.tk/portfolio/dns.tf
@@ -1,11 +1,11 @@
 resource "cloudflare_page_rule" "redirect_top" {
   zone_id  = var.zone_id         # ドメインのzone ID
-  target   = "https://boxp.tk/*" # リダイレクト元
+  target   = "https://boxp.tk*" # リダイレクト元
   priority = 2                   # ルールの優先度
 
   actions {
     forwarding_url {
-      url         = "https://www.b0xp.io/$1" # リダイレクト先URL
+      url         = "https://www.b0xp.io$1" # リダイレクト先URL
       status_code = 301                      # 301リダイレクト
     }
   }
@@ -13,12 +13,12 @@ resource "cloudflare_page_rule" "redirect_top" {
 
 resource "cloudflare_page_rule" "redirect_www" {
   zone_id  = var.zone_id             # ドメインのzone ID
-  target   = "https://www.boxp.tk/*" # リダイレクト元
+  target   = "https://www.boxp.tk*" # リダイレクト元
   priority = 1                       # ルールの優先度
 
   actions {
     forwarding_url {
-      url         = "https://www.b0xp.io/$1" # リダイレクト先URL
+      url         = "https://www.b0xp.io$1" # リダイレクト先URL
       status_code = 301                      # 301リダイレクト
     }
   }
