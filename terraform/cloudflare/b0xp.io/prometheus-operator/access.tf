bc7bf9f2eb064ba6e4d0bdad1037a8e4419d2283
boxp <tiyotiyouda@gmail.com>
2025-01-04 18:19:39 +0900

    add: prometheus-web.b0xp.io

--- terraform/cloudflare/b0xp.io/prometheus-operator/access.tf
+++ terraform/cloudflare/b0xp.io/prometheus-operator/access.tf
@@ -6,6 +6,14 @@ resource "cloudflare_access_application" "grafana" {
   session_duration = "24h"
 }
 
+# Creates an Access application to control who can connect.
+resource "cloudflare_access_application" "prometheus-web" {
+  zone_id          = var.zone_id
+  name             = "Access application for prometheus-web.b0xp.io"
+  domain           = "prometheus-web.b0xp.io"
+  session_duration = "24h"
+}
+
 data "cloudflare_access_identity_provider" "github" {
   zone_id = var.zone_id
   name    = "GitHub"
@@ -16,6 +24,18 @@ resource "cloudflare_access_policy" "grafana_policy" {
   application_id = cloudflare_access_application.grafana.id
   zone_id        = var.zone_id
   name           = "policy for grafana.b0xp.io"
+  precedence     = "1"
+  decision       = "allow"
+  include {
+    login_method = [data.cloudflare_access_identity_provider.github.id]
+  }
+}
+
+# Creates an Access policy for the application.
+resource "cloudflare_access_policy" "grafana_policy" {
+  application_id = cloudflare_access_application.prometheus_web.id
+  zone_id        = var.zone_id
+  name           = "policy for prometheus-web.b0xp.io"
   precedence     = "1"
   decision       = "allow"
   include {
