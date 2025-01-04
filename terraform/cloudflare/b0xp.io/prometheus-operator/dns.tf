44813213623ecf2ccddf82dd5a2846f4fe0cbdb4
boxp <tiyotiyouda@gmail.com>
2025-01-04 18:30:03 +0900

    fix: name

--- terraform/cloudflare/b0xp.io/prometheus-operator/dns.tf
+++ terraform/cloudflare/b0xp.io/prometheus-operator/dns.tf
@@ -8,7 +8,7 @@ resource "cloudflare_record" "grafana" {
 }
 
 # Creates the CNAME record that routes prometheus-web.b0xp.io to the tunnel.
-resource "cloudflare_record" "prometheus-web" {
+resource "cloudflare_record" "prometheus_web" {
   zone_id = var.zone_id
   name    = "prometheus-web"
   value   = cloudflare_tunnel.prometheus_operator_tunnel.cname
